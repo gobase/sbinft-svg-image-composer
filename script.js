32917d78b7535ddd3a4ad04e79cfeedba28d7097
Sarveshwar Singh <s.singh@sbinft.co.jp>
2024-08-30 14:25:47 +0900

    new impl

--- script.js
+++ script.js
@@ -1,196 +1,172 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let backgroundImageInput = document.getElementById("background-image");
-  let foregroundImagesContainer = document.getElementById("foreground-images");
-  let textInputsContainer = document.getElementById("text-inputs");
-  let previewContainer = document.getElementById("preview");
-  let addForegroundBtn = document.getElementById("add-foreground");
-  let addTextBtn = document.getElementById("add-text");
-  let downloadBtn = document.getElementById("download-svg");
+  const bgInput = document.getElementById("bg-image");
+  const fgInput = document.getElementById("fg-image");
+  const textInput = document.getElementById("text-input");
+  const fontSelect = document.getElementById("font-select");
+  const fontSize = document.getElementById("font-size");
+  const opacityRange = document.getElementById("opacity-range");
+  const apiEndpoint = document.getElementById("api-endpoint");
+  const previewArea = document.getElementById("preview-area");
 
-  let foregroundImagesCount = 0;
-  let textInputsCount = 0;
+  let background,
+    foregrounds = [],
+    texts = [];
 
-  backgroundImageInput.addEventListener("change", updatePreview);
-  addForegroundBtn.addEventListener("click", addForegroundImage);
-  addTextBtn.addEventListener("click", addTextInput);
-  downloadBtn.addEventListener("click", downloadSVG);
+  bgInput.addEventListener("change", handleBgUpload);
+  fgInput.addEventListener("change", handleFgUpload);
+  document.getElementById("add-text").addEventListener("click", addText);
+  document
+    .getElementById("generate-svg")
+    .addEventListener("click", generateSVG);
 
-  function updatePreview() {
-    // Create the base SVG
-    let svgNS = "http://www.w3.org/2000/svg";
-    let svg = document.createElementNS(svgNS, "svg");
+  function handleBgUpload(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        background = e.target.result;
+        renderPreview();
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function handleFgUpload(event) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        foregrounds.push({
+          src: e.target.result,
+          opacity: opacityRange.value / 100,
+          x: 0,
+          y: 0,
+        });
+        renderPreview();
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function addText() {
+    texts.push({
+      content: textInput.value,
+      font: fontSelect.value,
+      size: fontSize.value,
+      color: "#000000",
+      x: 0,
+      y: 0,
+    });
+    renderPreview();
+  }
+
+  function renderPreview() {
+    previewArea.innerHTML = "";
+    if (background) {
+      const bgImg = document.createElement("img");
+      bgImg.src = background;
+      bgImg.style.width = "100%";
+      previewArea.appendChild(bgImg);
+    }
+
+    foregrounds.forEach((fg, index) => {
+      const fgImg = document.createElement("img");
+      fgImg.src = fg.src;
+      fgImg.style.opacity = fg.opacity;
+      fgImg.style.left = `${fg.x}px`;
+      fgImg.style.top = `${fg.y}px`;
+      fgImg.draggable = true;
+      fgImg.ondragend = (e) => {
+        fg.x = e.clientX - previewArea.offsetLeft;
+        fg.y = e.clientY - previewArea.offsetTop;
+        renderPreview();
+      };
+      previewArea.appendChild(fgImg);
+    });
+
+    texts.forEach((txt, index) => {
+      const txtElem = document.createElement("div");
+      txtElem.textContent = txt.content;
+      txtElem.style.position = "absolute";
+      txtElem.style.fontFamily = txt.font;
+      txtElem.style.fontSize = `${txt.size}px`;
+      txtElem.style.color = txt.color;
+      txtElem.style.left = `${txt.x}px`;
+      txtElem.style.top = `${txt.y}px`;
+      txtElem.draggable = true;
+      txtElem.ondragend = (e) => {
+        txt.x = e.clientX - previewArea.offsetLeft;
+        txt.y = e.clientY - previewArea.offsetTop;
+        renderPreview();
+      };
+      previewArea.appendChild(txtElem);
+    });
+  }
+
+  function generateSVG() {
+    const svgNamespace = "http://www.w3.org/2000/svg";
+    const svg = document.createElementNS(svgNamespace, "svg");
     svg.setAttribute("width", "500");
     svg.setAttribute("height", "500");
-    svg.setAttribute("viewBox", "0 0 500 500");
 
-    // Add background image
-    let backgroundImage = backgroundImageInput.files[0];
-    if (backgroundImage) {
-      let reader = new FileReader();
-      reader.onload = function (e) {
-        let image = document.createElementNS(svgNS, "image");
-        image.setAttributeNS(
-          "http://www.w3.org/1999/xlink",
-          "href",
-          e.target.result
-        );
-        image.setAttribute("width", "100%");
-        image.setAttribute("height", "100%");
-        svg.appendChild(image);
-      };
-      reader.readAsDataURL(backgroundImage);
-    }
-
-    // Add foreground images
-    let foregroundImages = document.querySelectorAll(".foreground-image-input");
-    foregroundImages.forEach(function (input, index) {
-      let file = input.files[0];
-      if (file) {
-        let reader = new FileReader();
-        reader.onload = function (e) {
-          let image = document.createElementNS(svgNS, "image");
-          image.setAttributeNS(
-            "http://www.w3.org/1999/xlink",
-            "href",
-            e.target.result
-          );
-          image.setAttribute("x", "50");
-          image.setAttribute("y", "50");
-          image.setAttribute("width", "100");
-          image.setAttribute("height", "100");
-          image.setAttribute("opacity", input.dataset.opacity || 1);
-          image.style.cursor = "move";
-          image.classList.add("draggable");
-          svg.appendChild(image);
-        };
-        reader.readAsDataURL(file);
-      }
-    });
-
-    // Add text inputs
-    let textInputs = document.querySelectorAll(".text-input-group");
-    textInputs.forEach(function (group) {
-      let textElement = document.createElementNS(svgNS, "text");
-      textElement.textContent = group.querySelector(".text-input").value;
-      textElement.setAttribute("x", "100");
-      textElement.setAttribute("y", "100");
-      textElement.setAttribute(
-        "font-size",
-        group.querySelector(".font-size").value + "px"
+    if (background) {
+      const bgImg = document.createElementNS(svgNamespace, "image");
+      bgImg.setAttributeNS(
+        "http://www.w3.org/1999/xlink",
+        "xlink:href",
+        background
       );
-      textElement.setAttribute(
-        "fill",
-        group.querySelector(".text-color").value
+      bgImg.setAttribute("width", "500");
+      bgImg.setAttribute("height", "500");
+      svg.appendChild(bgImg);
+    }
+
+    foregrounds.forEach((fg, index) => {
+      const fgImg = document.createElementNS(svgNamespace, "image");
+      fgImg.setAttributeNS(
+        "http://www.w3.org/1999/xlink",
+        "xlink:href",
+        fg.src
       );
-      textElement.style.fontWeight = group.querySelector(".bold").checked
-        ? "bold"
-        : "normal";
-      textElement.style.fontStyle = group.querySelector(".italic").checked
-        ? "italic"
-        : "normal";
-      textElement.style.textDecoration = group.querySelector(".strikethrough")
-        .checked
-        ? "line-through"
-        : "none";
-      textElement.style.cursor = "move";
-      textElement.classList.add("draggable");
-      svg.appendChild(textElement);
+      fgImg.setAttribute("x", fg.x);
+      fgImg.setAttribute("y", fg.y);
+      fgImg.setAttribute("opacity", fg.opacity);
+      svg.appendChild(fgImg);
     });
 
-    // Clear previous preview and add the new SVG
-    while (previewContainer.firstChild) {
-      previewContainer.removeChild(previewContainer.firstChild);
-    }
-    previewContainer.appendChild(svg);
-
-    // Enable drag-and-drop functionality
-    enableDragAndDrop();
-  }
-
-  function enableDragAndDrop() {
-    let draggables = document.querySelectorAll(".draggable");
-    draggables.forEach(function (element) {
-      element.addEventListener("mousedown", function (e) {
-        let offsetX = e.clientX - element.getBoundingClientRect().left;
-        let offsetY = e.clientY - element.getBoundingClientRect().top;
-
-        function onMouseMove(e) {
-          element.setAttribute("x", e.clientX - offsetX);
-          element.setAttribute("y", e.clientY - offsetY);
-        }
-
-        document.addEventListener("mousemove", onMouseMove);
-
-        document.addEventListener(
-          "mouseup",
-          function () {
-            document.removeEventListener("mousemove", onMouseMove);
-          },
-          { once: true }
-        );
-      });
+    texts.forEach((txt, index) => {
+      const text = document.createElementNS(svgNamespace, "text");
+      text.textContent = txt.content;
+      text.setAttribute("x", txt.x);
+      text.setAttribute("y", txt.y);
+      text.setAttribute("font-family", txt.font);
+      text.setAttribute("font-size", txt.size);
+      text.setAttribute("fill", txt.color);
+      svg.appendChild(text);
     });
-  }
 
-  function addForegroundImage() {
-    foregroundImagesCount++;
-    let div = document.createElement("div");
-    div.innerHTML = `
-            <input type="file" class="foreground-image-input" accept="image/*" data-index="${foregroundImagesCount}">
-            <label for="opacity-${foregroundImagesCount}">Opacity:</label>
-            <input type="range" min="0" max="1" step="0.01" id="opacity-${foregroundImagesCount}" class="opacity-input" value="1">
-        `;
-    div
-      .querySelector(".foreground-image-input")
-      .addEventListener("change", updatePreview);
-    div.querySelector(".opacity-input").addEventListener("input", function () {
-      div.querySelector(".foreground-image-input").dataset.opacity = this.value;
-      updatePreview();
-    });
-    foregroundImagesContainer.appendChild(div);
-  }
+    const cdataScript = document.createElementNS(svgNamespace, "script");
+    cdataScript.setAttribute("type", "text/javascript");
+    const cdata = `
+        <![CDATA[
+            fetch('${apiEndpoint.value}')
+            .then(response => response.json())
+            .then(data => {
+                const attributes = data.attributes;
+                attributes.forEach(attr => {
+                    if (attr.trait_type === 'usage_count' && attr.value > 0) {
+                        // Show image and text if condition is met
+                    } else {
+                        // Hide image and text if condition is not met
+                    }
+                });
+            });
+        ]]>`;
+    cdataScript.appendChild(document.createTextNode(cdata));
+    svg.appendChild(cdataScript);
 
-  function addTextInput() {
-    textInputsCount++;
-    let div = document.createElement("div");
-    div.classList.add("text-input-group");
-    div.innerHTML = `
-            <input type="text" class="text-input" placeholder="Enter text">
-            <label for="font-size-${textInputsCount}">Font Size:</label>
-            <input type="number" id="font-size-${textInputsCount}" class="font-size" value="16">
-            <label for="text-color-${textInputsCount}">Color:</label>
-            <input type="color" id="text-color-${textInputsCount}" class="text-color" value="#000000">
-            <label><input type="checkbox" class="bold"> Bold</label>
-            <label><input type="checkbox" class="italic"> Italic</label>
-            <label><input type="checkbox" class="strikethrough"> Strikethrough</label>
-            <button class="remove-text">Remove</button>
-        `;
-    div.querySelector(".text-input").addEventListener("input", updatePreview);
-    div.querySelectorAll("input, select").forEach(function (input) {
-      input.addEventListener("change", updatePreview);
-    });
-    div.querySelector(".remove-text").addEventListener("click", function () {
-      div.remove();
-      updatePreview();
-    });
-    textInputsContainer.appendChild(div);
-  }
-
-  function downloadSVG() {
-    let svgElement = previewContainer.querySelector("svg");
-    if (svgElement) {
-      let serializer = new XMLSerializer();
-      let source = serializer.serializeToString(svgElement);
-
-      let svgBlob = new Blob([source], { type: "image/svg+xml;charset=utf-8" });
-      let url = URL.createObjectURL(svgBlob);
-
-      let downloadLink = document.createElement("a");
-      downloadLink.href = url;
-      downloadLink.download = "composed-image.svg";
-      downloadLink.click();
-
-      URL.revokeObjectURL(url);
-    }
+    previewArea.innerHTML = "";
+    previewArea.appendChild(svg);
   }
 });
